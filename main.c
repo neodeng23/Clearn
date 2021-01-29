9c5e6ee8cb7ac5b5b2767e49ca29eb8620fefd84
Neo_Deng <863494228@qq.com>
2021-01-29 10:03:54 +0800

    #202101029

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 #define WIDTH  5
 #define NEWLINE '\n'
 int count ;
+int max(int num1, int num2);
 
 void write_extern(void)
 {
     printf("count is %d\n", count);
 }
-
 
 int main() {
     /* 我的第一个 C 程序 */
@@ -19,15 +19,16 @@ int main() {
     int a, b;    /* 一次声明多个变量，用逗号分隔 */
     int c;
     float f;
+    char *d = "abcd";
 
     /* 给变量赋值 */
     a = 10;
     b = 20;
-
     c = a + b;
+
     printf("value of c : %d \n", c);
     f = 70.0/3.0;
-    printf("value of f : %f \n", f);
+    printf("value of f : %.1f \n", f);
 
     printf("Hello, World!\n");
     printf("Storage size for int : %d \n", sizeof(int));
@@ -36,5 +37,22 @@ int main() {
     count = 5;
     write_extern();
 
+    printf("%s \n", d);
+    printf("%sd \n", d);
+
     return 0;
+}
+
+/* 函数返回两个数中较大的那个数 */
+int max(int num1, int num2)
+{
+    /* 局部变量声明 */
+    int result;
+
+    if (num1 > num2)
+        result = num1;
+    else
+        result = num2;
+
+    return result;
 }
